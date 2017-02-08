100ebae02bba18ff2e47b54e88aa2ff674585636
KingOfLinux <shawnco789@gmail.com>
2017-02-07 19:22:20 -0500

    Update

--- lab2a/lab2a/code/filter.c
+++ lab2a/lab2a/code/filter.c
@@ -1,7 +1,7 @@
 /*-------------------------------------------------------------------
 File: filter.c
 
-Description:  Quick and dirty code. In particular, gets() is not a 
+Description:  Quick and dirty code. In particular, gets() is not a
               good function to use since it can write past the end
               of the memory buffer it receives.
               This program is specific to processing output received
@@ -18,14 +18,14 @@ int main(int argc, char **argv)
 
     state[0] = oldstate[0] = 0;
     /* Simply copy the first five lines */
-    for(i=0; i<5; i++) 
+    for(i=0; i<5; i++)
     {
         if (gets(buffer)==NULL)
             exit(-1); /* procmon produces 5 header lines - fatal error */
         else puts(buffer);
     }
 
-    while(gets(buffer)) 
+    while(gets(buffer))
     {
         sscanf(buffer, "%*s %s", state);
         if (strcmp(state, oldstate))  /* copy only if different from oldstate */
--- lab2a/lab2a/mon2.c
+++ lab2a/lab2a/mon2.c
@@ -20,6 +20,7 @@ Description: This program creates a process to run the program identified
 #include <fcntl.h>
 
 
+
 /* It's all done in main */
 int main(int argc, char **argv)
 {
@@ -37,27 +38,27 @@ int main(int argc, char **argv)
     /* First Step: Create the first process to run the program from the command line */
     execl(program, program, NULL);
 
-    pid_t parentPID = getppid();
 
-    printf("this is parent PID: %d \n", parentPID);
     /* Second step: Create the pipe to be used for connecting procmon to filter */
     pid_t child = fork();
-    printf("childPID: %d \n", child);
     if (child == -1){
       perror("fork");
       exit(1);
     }else if(child == 0){
-      printf("this is the childPID: %d \n", child);
+      /* Third step: Lets create the filter process - don't forget to connect to the pipe */
+      printf("procmon process \n");
+      execl("filter", "filter", NULL);
+      printf("child process execution \n");
     }else{
-      //execl("filter", "filter", NULL);
-      printf("PID after filter execution %d \n", getpid());
+      /* Fourth step: Lets create the procmon process - don't forget to connect to the pipe */
+      printf("procmon process. \n");
+      execl("procmon", "procmon", NULL);
+      printf("Parent process execution \n");
     }
 
-    /* Third step: Lets create the filter process - don't forget to connect to the pipe */
 
-    /* Fourth step: Lets create the procmon process - don't forget to connect to the pipe */
-    //execl("procmon", "procmon", NULL);
-    printf("running procmon \n");
+
+
     /* Fifth step: Let things run for 20 seconds */
     sleep(20);
     /* Last step:
