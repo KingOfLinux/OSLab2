a8dc9d97bae090338acda604a5e3bf68cbbb9b2a
KingOfLinux <shawnco789@gmail.com>
2017-02-09 16:50:48 -0500

    truly fixed the permission problem

--- lab2a/lab2a/mon2.c
+++ lab2a/lab2a/mon2.c
@@ -20,7 +20,6 @@ Description: This program creates a process to run the program identified
 #include <fcntl.h>
 
 
-
 /* It's all done in main */
 int main(int argc, char **argv)
 {
@@ -38,27 +37,27 @@ int main(int argc, char **argv)
     /* First Step: Create the first process to run the program from the command line */
     execl(program, program, NULL);
 
+    pid_t parentPID = getppid();
 
+    printf("this is parent PID: %d \n", parentPID);
     /* Second step: Create the pipe to be used for connecting procmon to filter */
     pid_t child = fork();
+    printf("childPID: %d \n", child);
     if (child == -1){
       perror("fork");
       exit(1);
     }else if(child == 0){
-      /* Third step: Lets create the filter process - don't forget to connect to the pipe */
-      printf("filter process \n");
-      int test = execl("filter", "filter", NULL);
-      printf("child process execution %d \n", test);
+      printf("this is the childPID: %d \n", child);
     }else{
-      /* Fourth step: Lets create the procmon process - don't forget to connect to the pipe */
-      printf("procmon process. \n");
-      int test2 = execl("procmon", "procmon", NULL);
-      printf("Parent process execution %d  \n",test2);
+      //execl("filter", "filter", NULL);
+      printf("PID after filter execution %d \n", getpid());
     }
 
+    /* Third step: Lets create the filter process - don't forget to connect to the pipe */
 
-
-
+    /* Fourth step: Lets create the procmon process - don't forget to connect to the pipe */
+    //execl("procmon", "procmon", NULL);
+    printf("running procmon \n");
     /* Fifth step: Let things run for 20 seconds */
     sleep(20);
     /* Last step:
